3148f399a5cd122d2c86ddd53b6f97f9b6f4c15c
emonibnsalim <emonibnsali@gmail.com>
2021-12-24 00:30:56 +0600

    added logo

--- src/Components/allAuth/firebaseConfig.js
+++ src/Components/allAuth/firebaseConfig.js
@@ -1,9 +1,4 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDSm207zB_o-WRkTdtXanpRDgTSjaM5DHM",
-    authDomain: "your-house-helper-602ab.firebaseapp.com",
-    projectId: "your-house-helper-602ab",
-    storageBucket: "your-house-helper-602ab.appspot.com",
-    messagingSenderId: "18936668086",
-    appId: "1:18936668086:web:119a2f09ef388adcfdeb17"
+   
 };
 export default firebaseConfig;
